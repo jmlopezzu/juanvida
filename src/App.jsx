3abdebd26aa42e3d8d84602dc1e090306e648a8f
jmlopezzu <jmlopezzu@unal.edu.co>
2025-01-14 17:27:46 -0500

    update: favicon and home

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function LandingPage() {
     <div className="flex flex-col min-h-screen">
       <header className="px-4 lg:px-6 h-14 flex items-center">
         <Link className="flex items-center justify-center" href="#">
-        <img src={JV} alt="Juan Vida Yoga" className="h-12 mt-" />
+        <img src={JV} alt="Juan Vida Yoga" className="h-12 mt-12" />
         </Link>
         <nav className="ml-auto flex gap-4 sm:gap-6">
           <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
